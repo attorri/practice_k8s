ab0354faee7316bcbb8facf630037daaf12d1c71
Christian Attorri <christian.attorri@gsa.gov>
2025-03-13 12:16:39 -0400

    first flask app, PLUS I ran it on k8s :)

--- frontend/main.py
+++ frontend/main.py
@@ -1,1 +1,18 @@
-print('hello world')
+from flask import Flask, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return 'hello, this is the main page :) <a href="https://deepseek.com">DeepSeek</a>'
+
+@app.route('/<name>')
+def user(name):
+    return 'Hello {}!'.format(name)
+
+@app.route('/admin')
+def admin():
+    return redirect(url_for('home'))
+
+if __name__ == '__main__':
+    app.run(host='0.0.0.0',port=5000)
